0f63c5fcd0b52feb00fed871297fa44debcd82ce
Victor Soares <victor_soares@live.com>
2025-04-21 05:11:53 -0300

    feat: redirect root path to swagger docs

--- app/api/routes/router.py
+++ app/api/routes/router.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter
+from fastapi.responses import RedirectResponse
 
 from app.api.routes.health import router as health_router
 from app.api.routes.mms import router as mms_router
@@ -6,3 +7,8 @@ from app.api.routes.mms import router as mms_router
 router = APIRouter()
 router.include_router(mms_router)
 router.include_router(health_router)
+
+
+@router.get("/", include_in_schema=False)
+async def root():
+    return RedirectResponse(url="/docs")
